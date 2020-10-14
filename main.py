9f4c008727e81a0fb5fae3a0aef6ffef6b60646b
Luciano Muñoz <luchisds@gmail.com>
2020-10-14 08:31:08 -0300

    First steps with firebase integration

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import time
-import logging
+import datetime
 import tweepy
+import firebase_admin
+from firebase_admin import credentials, db
 
 from dotenv import load_dotenv
-
-logger = logging.getLogger()
 
 load_dotenv()
 
@@ -14,6 +14,17 @@ API_KEY = os.environ['API_KEY']
 API_KEY_SECRET = os.environ['API_KEY_SECRET']
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+
+# Firebase connection
+GOOGLE_APPLICATION_CREDENTIALS = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+FIREBASE_DATABASE_URL = os.environ['FIREBASE_DATABASE_URL']
+firebase_credentials = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS)
+firebase_admin.initialize_app(firebase_credentials, {
+    'databaseURL': FIREBASE_DATABASE_URL
+})
+db_root_ref = db.reference('/')
+db_boards_ref = db.reference('Boards')
+
 
 # Board matrix
 board = [
@@ -58,26 +69,99 @@ def authenticate():
     return api
 
 
-def check_mentions(api, keywords, since_id):
-    logger.info('Retrieving mentions')
-    new_since_id = since_id
-    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
-        new_since_id = max(tweet.id, new_since_id)
+def check_mentions(api, keywords, last_mention):
+    print('Checking mentions...')
+    new_last_mention = None
+    # api.mentions_timeline --> Returns the 20 most recent mentions, including retweets
+    # 4 - CHECK MENTIONS
+    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=last_mention).items():
+        new_last_mention = tweet.id
         if tweet.in_reply_to_status_id is not None:
             continue
+        # 5 - CHECK IF TEXT CONTAIN ANY OF THE KEYWORDS 
         if any(keyword in tweet.text.lower() for keyword in keywords):
-            logger.info(f'Answering to {tweet.user.name}')
-            api.update_status(status='Ok, challenge accepted. To start playing reply with row (a,b,c) and col (1,2,3) like b1, a2, c0...', in_reply_to_status_id=tweet.id,)
-            start_game()
-    return new_since_id
+            # 6 - CHECK IF EXISTS A PREVIOUS GAME
+            game_exists = check_existing_game(tweet.user.name)
+            if not game_exists:
+                # 7 - IF NOT EXISTS A PREVIOUS GAME, START A NEW ONE IF KEYWORD IS "START"
+                if keyword == 'start':
+                    start_game(tweet.user.name, tweet.id, api)
+                else:
+                    print('You didn\'t start any game')
+            else:
+                print('TODO - Move')
+        else:
+            print('Command not recognized')
+    return new_last_mention
+
+
+def start_game(username, id, api):
+    # 8 - SAVE NEW GAME DATA
+    data = {
+        'id': id,
+        'date': datetime.datetime.now(),
+        'status': 'playing'
+    }
+    set_db(db_boards_ref, username, data)
+    # 9 - REPLY WITH INSTRUCTIONS
+    api.update_status(status='Ok, challenge accepted. To start playing reply with row (a,b,c) and col (1,2,3) like b1, a2, c0...', in_reply_to_status_id=id, auto_populate_reply_metadata=True)
+    print('Reply done')
+
+
+
+def check_existing_game(username):
+    return db_boards_ref.child(username).order_by_child('status').equal_to('playing').get()
+
+
+def get_last_mention():
+    last_mention = get_db(db_root_ref, 'last_mention_id')
+    return last_mention[0]['last_mention_id'] or 1
+
+
+def update_last_mention(id):
+    return set_db(db_root_ref, 'last_mention_id', id)
+
+
+def get_db(ref, key):
+    return ref.get(key)
+
+
+def set_db(ref, key, data):
+    return ref.set({
+        key: data
+    })
+    # ref.set({
+    #     'Employee': {
+    #         'emp1': {
+    #             'name': 'Parwiz',
+    #             'lname': 'Forogh',
+    #             'age': 24
+    #         },
+    #         'emp2': {
+    #             'name': 'Orgoth',
+    #             'lname': 'Margh',
+    #             'age': 30
+    #         }
+    #     }
+    # })
 
 
 def main():
+    last_mention = None
+    keywords = ['start','a1','a2','a3','b1','b2','b3','c1','c2','c3']
     api = authenticate()
-    since_id = 1
     while True:
-        since_id = check_mentions(api, ['start','a1','a2','a3','b1','b2','b3','c1','c2','c3'], since_id)
-        logger.info('Waiting...')
+        # 1 - CHECK LAST MENTION
+        if not last_mention:
+            last_mention = get_last_mention()
+            print('Last Mention:', last_mention)
+        # 2 - CHECK NEW MENTIONS
+        new_last_mention = check_mentions(api, keywords, last_mention)
+        # 3 - UPDATE LAST MENTION IF THERE ARE NEW MENTIONS
+        if new_last_mention != last_mention and new_last_mention is not None:
+            update_last_mention(new_last_mention)
+            last_mention = new_last_mention
+
         time.sleep(60)
 
 
